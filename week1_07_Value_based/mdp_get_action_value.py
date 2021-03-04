cbb3880876e50314314c163cffedf4441391677b
Natalia Luneva <61375514+lwtztea@users.noreply.github.com>
2021-03-04 22:06:34 +0300

    undo the changes

--- week1_07_Value_based/mdp_get_action_value.py
+++ week1_07_Value_based/mdp_get_action_value.py
@@ -1,14 +1,9 @@
-
 def get_action_value(mdp, state_values, state, action, gamma):
     """ Computes Q(s,a) as in formula above """
 
+    accumulator = 0.
     # YOUR CODE HERE
-    results = 0.
-    for posible_state in mdp.get_all_states():
-        transition_prob = mdp.get_transition_prob(state, action, posible_state)
-        reward = mdp.get_reward(state, action, posible_state)
-        state_value = state_values[posible_state]
-
-        results += transition_prob * (reward + gamma * state_value)
-
-    return results # < YOUR CODE >
+    for next_state, prob in mdp.get_next_states(state, action).items():
+        transition_reward = mdp.get_reward(state, action, next_state)
+        accumulator += prob * (transition_reward + gamma * state_values[next_state])
+    return accumulator

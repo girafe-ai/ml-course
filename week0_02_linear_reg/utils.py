791d39c045c60ff4725a019b836496f434f4541f
Vladislav Goncharenko <vladislav.goncharenko@phystech.edu>
2021-06-27 18:11:21 +0300

    more enhancements

--- week0_02_linear_reg/utils.py
+++ week0_02_linear_reg/utils.py
@@ -2,12 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-# Move to utils.py
 def visualise(mu, sigma, points, title=None, greater_than_zero=False, log_scale=False):
     if greater_than_zero:
         mu = np.clip(mu, 0, mu.max())
 
-    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+    _, ax = plt.subplots(1, 1, figsize=(15, 10))
     plt.plot(points, mu, "r", label="mean value")
     _x = np.concatenate((points, points[::-1]))
     _y = np.concatenate(

997d2f240087dc03d1fabc3492696c6b486b5305
Radoslav Neychev <neychev@phystech.edu>
2022-10-08 13:36:04 +0300

    added hw01 materials

---
+++ homeworks/hw01_pytorch_basics/template0101.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch
+from torch import nn
+
+def create_model():
+    # your code here
+    # return model instance (None is just a placeholder)
+
+    return None
+
+def count_parameters(model):
+    # your code here
+    # return integer number (None is just a placeholder)
+    
+    return None
+    

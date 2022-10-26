0057a65943c7d04cc5fa226dd3decf0c58811af6
Татьяна Гайнцева <atmyre@192.168.1.112>
2022-10-26 23:24:15 +0300

    4hw

---
+++ homeworks/hw04_cnn/README.md
@@ -0,0 +1,3 @@
+Third assignment:
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1NUlMWF6iim4P94ZyVdIT7WQauXRBrPTq?usp=sharing)
+
---
+++ homeworks/hw04_cnn/submission_template04.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        ...
+
+    
+    def forward(self, x):
+        ...
+
+def create_model():
+    return ConvNet()

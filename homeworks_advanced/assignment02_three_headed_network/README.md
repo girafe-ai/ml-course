3da62afe410b5977e38a0255d61222d3ede8fbdd
Radoslav Neychev <neychev@phystech.edu>
2023-02-13 23:53:43 +0300

    added assignment 02

---
+++ homeworks_advanced/assignment02_three_headed_network/README.md
@@ -0,0 +1,2 @@
+Assignment on more complex network:
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/girafe-ai/ml-course/blob/23s_advanced/homeworks_advanced/assignment02_three_headed_network/assignment02_three_headed_network.ipynb)
---
+++ homeworks_advanced/assignment02_three_headed_network/network.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+import tqdm
+
+
+class ThreeInputsNet(nn.Module):
+    def __init__(self, n_tokens, n_cat_features, concat_number_of_features, hid_size=64):
+        super(ThreeInputsNet, self).__init__()
+        self.title_emb = nn.Embedding(n_tokens, embedding_dim=hid_size)
+        # <YOUR CODE HERE>        
+        
+        self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
+        # <YOUR CODE HERE>
+        
+        self.category_out = # <YOUR CODE HERE>
+
+
+        # Example for the final layers (after the concatenation)
+        self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2)
+        self.final_dense = nn.Linear(in_features=hid_size*2, out_features=1)
+
+        
+
+    def forward(self, whole_input):
+        input1, input2, input3 = whole_input
+        title_beg = self.title_emb(input1).permute((0, 2, 1))
+        title = # <YOUR CODE HERE>
+        
+        full_beg = self.full_emb(input2).permute((0, 2, 1))
+        full = # <YOUR CODE HERE>        
+        
+        category = # <YOUR CODE HERE>        
+        
+        concatenated = torch.cat(
+            [
+            title.view(title.size(0), -1),
+            full.view(full.size(0), -1),
+            category.view(category.size(0), -1)
+            ],
+            dim=1)
+        
+        out = # <YOUR CODE HERE>
+        
+        return out
